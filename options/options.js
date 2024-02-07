5b84cbc680ed963285d10801c305abc95a2ba991
Crowdbotics <team@crowdbotics.com>
2024-02-07 15:49:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "focus-friend-46059",
+  url: "https://focus-friend-46059.botics.co",
+  api: "https://focus-friend-46059.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
